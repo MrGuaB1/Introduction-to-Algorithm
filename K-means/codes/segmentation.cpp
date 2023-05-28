d96a16c00517176a38e7a2b3958c5ec68616cdca
Akira <127002578+MrGuaB1@users.noreply.github.com>
2023-05-28 20:33:13 +0800

    Update segmentation.cpp

--- K-means/codes/segmentation.cpp
+++ K-means/codes/segmentation.cpp
@@ -3,34 +3,39 @@
 
 using namespace cv;
 
-int main(int argc, char** argv)
+int main()
 {
-	std::cout << "Usage: ./executable image number_of_clusters\n";
-	if (argc != 3)
-		return 0;
+    std::string imagePath = "idol.jpg"; // 图像文件的路径
+    int clusterCount = 8; // 聚类数目
 
-	Mat src = imread(argv[1], 1);
-	Mat samples(src.rows * src.cols, 3, CV_32F);
-	for (int y = 0; y < src.rows; y++)
-		for (int x = 0; x < src.cols; x++)
-			for (int z = 0; z < 3; z++)
-				samples.at<float>(y + x * src.rows, z) = src.at<Vec3b>(y, x)[z];
+    Mat src = imread(imagePath, 1);
+    if (src.empty())
+    {
+        std::cout << "Failed to read image file: " << imagePath << std::endl;
+        return 0;
+    }
 
-	int clusterCount = std::stoi(argv[2]);
-	Mat labels;
-	int attempts = 5;
-	Mat centers;
-	kmeans(samples, clusterCount, labels, TermCriteria(cv::TermCriteria::COUNT | cv::TermCriteria::EPS, 10000, 0.0001), attempts, KMEANS_PP_CENTERS, centers);
+    Mat samples(src.rows * src.cols, 3, CV_32F);
+    for (int y = 0; y < src.rows; y++)
+        for (int x = 0; x < src.cols; x++)
+            for (int z = 0; z < 3; z++)
+                samples.at<float>(y + x * src.rows, z) = src.at<Vec3b>(y, x)[z];
 
-	Mat new_image(src.size(), src.type());
-	for (int y = 0; y < src.rows; y++)
-		for (int x = 0; x < src.cols; x++)
-		{
-			int cluster_idx = labels.at<int>(y + x * src.rows, 0);
-			new_image.at<Vec3b>(y, x)[0] = centers.at<float>(cluster_idx, 0);
-			new_image.at<Vec3b>(y, x)[1] = centers.at<float>(cluster_idx, 1);
-			new_image.at<Vec3b>(y, x)[2] = centers.at<float>(cluster_idx, 2);
-		}
-	imshow("clustered image", new_image);
-	waitKey(0);
+    Mat labels;
+    int attempts = 5;
+    Mat centers;
+    kmeans(samples, clusterCount, labels, TermCriteria(cv::TermCriteria::COUNT | cv::TermCriteria::EPS, 10000, 0.0001), attempts, KMEANS_PP_CENTERS, centers);
+
+    Mat new_image(src.size(), src.type());
+    for (int y = 0; y < src.rows; y++)
+        for (int x = 0; x < src.cols; x++)
+        {
+            int cluster_idx = labels.at<int>(y + x * src.rows, 0);
+            new_image.at<Vec3b>(y, x)[0] = centers.at<float>(cluster_idx, 0);
+            new_image.at<Vec3b>(y, x)[1] = centers.at<float>(cluster_idx, 1);
+            new_image.at<Vec3b>(y, x)[2] = centers.at<float>(cluster_idx, 2);
+        }
+
+    imshow("clustered image", new_image);
+    waitKey(0);
 }
