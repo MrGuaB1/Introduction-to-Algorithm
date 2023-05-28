4dda0ac96dfb9e29d84fba1a659e0d8d099b8083
Akira <127002578+MrGuaB1@users.noreply.github.com>
2023-05-28 19:22:21 +0800

    Add files via upload

---
+++ K-means/codes/segmentation.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+int main(int argc, char** argv)
+{
+	std::cout << "Usage: ./executable image number_of_clusters\n";
+	if (argc != 3)
+		return 0;
+
+	Mat src = imread(argv[1], 1);
+	Mat samples(src.rows * src.cols, 3, CV_32F);
+	for (int y = 0; y < src.rows; y++)
+		for (int x = 0; x < src.cols; x++)
+			for (int z = 0; z < 3; z++)
+				samples.at<float>(y + x * src.rows, z) = src.at<Vec3b>(y, x)[z];
+
+	int clusterCount = std::stoi(argv[2]);
+	Mat labels;
+	int attempts = 5;
+	Mat centers;
+	kmeans(samples, clusterCount, labels, TermCriteria(CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 10000, 0.0001), attempts, KMEANS_PP_CENTERS, centers);
+
+	Mat new_image(src.size(), src.type());
+	for (int y = 0; y < src.rows; y++)
+		for (int x = 0; x < src.cols; x++)
+		{
+			int cluster_idx = labels.at<int>(y + x * src.rows, 0);
+			new_image.at<Vec3b>(y, x)[0] = centers.at<float>(cluster_idx, 0);
+			new_image.at<Vec3b>(y, x)[1] = centers.at<float>(cluster_idx, 1);
+			new_image.at<Vec3b>(y, x)[2] = centers.at<float>(cluster_idx, 2);
+		}
+	imshow("clustered image", new_image);
+	waitKey(0);
+}
